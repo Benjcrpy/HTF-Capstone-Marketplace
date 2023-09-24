2a726fa9f65957b56fe3eee7f77fffe53de64778
Benj <93909966+Benjcrpy@users.noreply.github.com>
2023-09-24 22:28:26 +0800

    UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,76 @@
-import NextLink from 'next/link'
 import type { NextPage } from "next";
-import { Button, Container, Flex, Heading, Image, Stack } from '@chakra-ui/react';
+import Link from "next/link";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
+/**
+ * Landing page with a simple gradient background and a hero asset.
+ * Free to customize as you see fit.
+ */
 const Home: NextPage = () => {
   return (
-    <Container maxW={"1200px"}>
-      <Flex h={"80vh"} alignItems={"center"} justifyContent={"center"}>
-        <Stack spacing={4} align={"center"}>
-          <Heading>Marketplace</Heading>
-          <Button
-             as={NextLink} href='/buy'
-          >Shop NFTs</Button>
-        </Stack>
-      </Flex>
-    </Container>
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <div className={styles.hero}>
+          <div className={styles.heroBackground}>
+            <div className={styles.heroBackgroundInner}>
+              <Image
+                src="/hero-gradient.png"
+                width={1390}
+                height={1390}
+                alt="Background gradient from red to blue"
+                quality={100}
+                className={styles.gradient}
+              />
+            </div>
+          </div>
+          <div className={styles.heroAssetFrame}>
+            <Image
+              src="/hero-asset.png"
+              width={860}
+              height={540}
+              alt="Hero asset, NFT marketplace"
+              quality={100}
+              className={styles.heroAsset}
+            />
+          </div>
+          <div className={styles.heroBodyContainer}>
+            <div className={styles.heroBody}>
+              <h1 className={styles.heroTitle}>
+                <span className={styles.heroTitleGradient}>
+                  HAPPY THREE FRIENDS
+                </span>
+                <br />
+                MARKETPLACE
+              </h1>
+              <p className={styles.heroSubtitle}>
+                <Link
+                  className={styles.link}
+                  href="https://thirdweb.com"
+                  target="_blank"
+                >
+                  thirdweb
+                </Link>{" "}
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, earum dolorem vitae nemo laborum nihil eligendi omnis pariatur facere saepe fuga voluptatibus veritatis itaque qui debitis perferendis reprehenderit dolores placeat?
+              </p>
+
+              <div className={styles.heroCtaContainer}>
+                <Link className={styles.heroCta} href="/buy">
+                  SHOP
+                </Link>
+                <Link
+                  className={styles.secondaryCta}
+                  href="https://github.com/thirdweb-example/marketplace-v3"
+                  target="_blank"
+                >
+                  GitHub
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
