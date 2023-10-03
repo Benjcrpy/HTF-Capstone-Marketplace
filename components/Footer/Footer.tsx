636f9667400c9ef1b283975fc3f230d8677e195e
Benj <93909966+Benjcrpy@users.noreply.github.com>
2023-10-04 05:48:57 +0800

    Footer adjustment

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,22 +1,86 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import {
+  TiSocialFacebook,
+  TiSocialLinkedin,
+  TiSocialTwitter,
+  TiSocialYoutube,
+  TiSocialInstagram,
+ 
+} from "react-icons/ti";
+import { RiSendPlaneFill } from "react-icons/ri";
+
+// INTERNAL IMPORTS
+import Style from "./Footer.module.css";
 
 
+const Footer = () => {
+  return (
+    <div className={Style.footer}>
+      <div className={Style.footer_box}>
+        <div className={Style.footer_box_social}>
+         
+          <p>
+            The world’s first and largest digital marketplace for crypto collectibles and non-fungible tokens (NFTs).
+            Buy, sell, and discover exclusive digital items.
+          </p>
 
-const Footer = () =>{
-    return(
-        <div className="footer">
-            <div className="sb__footer section_padding">
-              <div className="sb__footer-links">
-                <div className="sb__footer-links-div">
-                    <h4>For Marketplace</h4>
-                    <a href="/marketplace">
-                        <p>Marketplace</p>
-                    </a>
-                </div>
-              </div>
-            </div>
+          {/* Social Section */}
+          <div className={Style.footer_social}>
+            <a href="#">
+              <TiSocialFacebook />
+            </a>
+            <a href="#">
+              <TiSocialLinkedin />
+            </a>
+            <a href="#">
+              <TiSocialTwitter />
+            </a>
+            <a href="#">
+              <TiSocialYoutube />
+            </a>
+            <a href="#">
+              <TiSocialInstagram />
+            </a>
+          </div>
         </div>
-    )
-}
+
+        {/* Menu Section */}
+        <div className={Style.footer_box_discover}>
+          <h3>Discover</h3>
+            <p>Build</p>
+            <p>Build</p>
+            <p>Build</p>
+            <p>Build</p>
+        </div>
+
+        <div className={Style.footer_box_helpcenter}>
+          <h3>Help Center</h3>
+            <p>Build</p>
+            <p>Build</p>
+            <p>Build</p>
+            <p>Build</p>
+        </div>
+
+        {/* Subscribe Section */}
+        <div className={Style.subscribe}>
+          <h3>Subscribe</h3>
+            
+          <div className={Style.subscribe_box}>
+            <input type="email" placeholder="Enter your email *" />
+            <RiSendPlaneFill className={Style.subscribe_box_send} />
+          </div>
+
+          <div className={Style.subscribe_box_info}>
+            <p> 
+                @{new Date().getFullYear()} HTF. All right reseved.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
