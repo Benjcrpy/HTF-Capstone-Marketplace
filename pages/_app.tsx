9e0395e0020aceb4af89bfb07b1b82c83c5132cb
Benj <93909966+Benjcrpy@users.noreply.github.com>
2023-12-05 05:29:24 +0800

    a

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,8 +10,6 @@ import "../styles/globals.css";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
 
- 
-
       <ThirdwebProvider 
       clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
       activeChain={NETWORK}
